44db61639055c7a71aa62abe597d22bd2a8ec4f4
Iman <hafiz.ghazman@gmail.com>
2018-11-28 15:05:57 +0000

    escape_characters has not been complete with unicode

--- escape_characters.py
+++ escape_characters.py
@@ -12,4 +12,27 @@ formfeed = '___\\f___'
 formfeed_result = '___\f___'
 linefeed = '___\n___'
 linefeed = '___\\n___'
+unicode_utf ='\\uxxxx'
+unicode_utf16 ='\\UXXXXXXXX'
+
+def arrowDown(a):
+    arrow0 = '\u2510'
+    arrow1 = '\u2502'
+    arrow2 = '\u2514'
+    arrowHead ='\u21E2'
+    padding = a+1
+    print('{0}\n{1:>{2}}\n{3:>{2}}{4}'.format(arrow0,arrow1,padding,arrow2,arrowHead), end='')
+
+def printing_hex():
+    '''
+    this function shows 
+    how to print hexadecimal number
+    '''
+    print("To print a hex number, use format\n")
+    this="print({0:#5x}.format(177)"
+    a=len(this)
+    print(this, end='')
+    arrowDown(a)
+    print('{0:#5x}'.format(177))
+
 
--- incomplete_filing.py
+++ incomplete_filing.py
@@ -6,14 +6,40 @@ This module is used to handle file:
 -appending
 """
 
-def write():
-    ''''''    
-def read():
-    ''''''
-def copy():
-    ''''''
+def write(data):
+    '''
+    To write a file
+    use nameHandle=open('data', 'w')
+    The same file name will have its content erased
+    '''
+    nameHandle = open(data, 'w')
+    nameHandle.write('Micheal\t12\n')
+    nameHandle.write('Greg\t13\n')
+    nameHandle.close()
+    nameHandle = open(data, 'r')
+    for line in nameHandle:
+        print(line[:-1])
+    nameHandle.close()
+
+def read(data):
+    '''
+    To read a file
+    '''
+    nameHandle = open(data, 'r')
+    for line in nameHandle:
+        print(nameHandle.readline())
+    nameHandle.close()
+def copy(a,b):
+    '''
+    Copy a file to a newfile
+    copy(a,b)
+    a is a str of file to copy
+    b is a str of file to create and hv content of a
+    '''
+
 def add():
-    ''''''
+    '''
+    '''
 def auto_rename():
     ''''''
 
