befdd686122cf2d6cb9c57b950e74b8f45ee8645
iman <hafiz.ghazman@gmail.com>
2019-02-07 12:04:37 +0800

    Add pptk visualization for data_iteration

--- data_iteration.py
+++ data_iteration.py
@@ -29,7 +29,7 @@ Data streaming from Kinect:
 import freenect, cv2
 import numpy as np
 import sympy as sym
-from kinect_test import *
+#from kinect_test import *
 from calibkinect import depth2xyzuv
 
 
@@ -83,13 +83,42 @@ def point_cloud():
     '''
     return depth2xyzuv(freenect.sync_get_depth()[0])
 
-#attempting visualization
+#attempting visualization FAIL
+#DO NOT USE mlab DIRECTLY WITH
+#RAW POINT CLOUDS
 from mayavi import mlab
+import pptk
 
-def point_cloud_visual():
+#def point_cloud_visual():
+#    '''
+#    Attempting visualization using mlab. just a screenshot [1 Feb 2019]
+#    DO NOT USE THIS METHOD
+#    '''
+#    xyz, uv = point_cloud()
+#    s = mlab.points3d(xyz[:,0],xyz[:,1],xyz[:,2])
+#    mlab.show()
+
+def pptk_point_cloud_visual(reload=False):
     '''
-    Attempting visualization using mlab. just a screenshot [1 Feb 2019]
+    Using PPTK package to visualize
+    point cloud directly
+    
+    use pptk.load(xyz) to reload the 
+    viewer with new set of point cloud
+    
+    To reload, set argument reload=True
     '''
-    xyz, uv = point_cloud()
-    s = mlab.points3d(xyz[:,0],xyz[:,1],xyz[:,2])
-    mlab.show()
+    if reload:
+        xyz, uv = point_cloud()
+        v.load(xyz)
+    else:
+        xyz, uv = point_cloud()
+        v = pptk.viewer(xyz)
+    return v
+def pptk_reload(v=pptk_point_cloud_visual()):
+    '''
+    Reload the point cloud
+    in a pptk viewer
+    '''
+    v.clear()
+    v.load(point_cloud()[0])
